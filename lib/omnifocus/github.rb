d2c746bfaaaf1d233bb03c5ace08306179a383f3
ajahammerly <aja.hammerly@gmail.com>
2011-05-31 21:27:24 -0800

    Added pulling issues from a user's organizations not just their personal repositories.  Pulls all open issues, from all repositories, from all organizations a user belongs to.
    [git-p4: depot-paths = "//src/omnifocus-github/dev/": change = 6279]

--- lib/omnifocus/github.rb
+++ lib/omnifocus/github.rb
@@ -13,6 +13,8 @@ module OmniFocus::Github
 
   def populate_github_tasks
     user  = `git config --global github.user`.chomp
+
+    # Personal projects
     projects = fetch("repos/show/#{user}", "repositories").select { |project|
       project[:open_issues] > 0
     }.map { |project|
@@ -32,6 +34,32 @@ module OmniFocus::Github
         end
 
         bug_db[project][ticket_id] = [title, url]
+      end
+    end
+
+    # Organization projects
+    orgs = fetch("user/show/#{user}/organizations", "organizations")
+    orgs.each do |o|
+      login = o["login"]
+      projects = fetch("repos/show/#{login}", "repositories")
+      projects = projects.select { |project| project[:open_issues] > 0}
+      projects = projects.map{|project| project[:name]}
+
+      projects.sort.each do |project|
+        issues = fetch("issues/list/#{login}/#{project}/open", "issues")
+        issues.each do |issue|
+          number    = issue["number"]
+          ticket_id = "GH-#{project}##{number}"
+          title     = "#{ticket_id}: #{issue["title"]}"
+          url       = "#{GH_URL}/#{login}/#{project}/issues/#issue/#{number}"
+
+          if existing[ticket_id] then
+            bug_db[existing[ticket_id]][ticket_id] = true
+            next
+          end
+
+          bug_db[project][ticket_id] = [title, url]
+        end
       end
     end
   end
