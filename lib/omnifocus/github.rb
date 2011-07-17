235b7c2c5cc26e84a7c57b19d422f02ce020cce3
Ryan Davis <ryand@zenspider.com>
2011-07-16 19:15:03 -0800

    + Added project progress output. - Now only adds tickets assigned to your user. [git-p4: depot-paths = "//src/omnifocus-github/dev/": change = 6440]

--- lib/omnifocus/github.rb
+++ lib/omnifocus/github.rb
@@ -4,7 +4,7 @@ require 'yaml'
 module OmniFocus::Github
   VERSION = '1.0.0'
 
-  GH_URL = "http://github.com"
+  GH_URL = "https://github.com"
 
   def fetch url, key
     base_url = "#{GH_URL}/api/v2/yaml"
@@ -22,11 +22,12 @@ module OmniFocus::Github
     }
 
     projects.sort.each do |project|
+      warn "  scanning #{user}/#{project}"
       fetch("issues/list/#{user}/#{project}/open", "issues").each do |issue|
         number    = issue["number"]
         ticket_id = "GH-#{project}##{number}"
         title     = "#{ticket_id}: #{issue["title"]}"
-        url       = "#{GH_URL}/#{user}/#{project}/issues/#issue/#{number}"
+        url       = "#{GH_URL}/#{user}/#{project}/issues/#{number}"
 
         if existing[ticket_id] then
           bug_db[existing[ticket_id]][ticket_id] = true
@@ -46,13 +47,16 @@ module OmniFocus::Github
       projects = projects.map{|project| project[:name]}
 
       projects.sort.each do |project|
+        warn "  scanning #{login}/#{project}"
         issues = fetch("issues/list/#{login}/#{project}/open", "issues")
         issues.each do |issue|
           number    = issue["number"]
+          t_user    = issue["user"]
           ticket_id = "GH-#{project}##{number}"
           title     = "#{ticket_id}: #{issue["title"]}"
           url       = "#{GH_URL}/#{login}/#{project}/issues/#issue/#{number}"
 
+          next unless t_user == user
           if existing[ticket_id] then
             bug_db[existing[ticket_id]][ticket_id] = true
             next
