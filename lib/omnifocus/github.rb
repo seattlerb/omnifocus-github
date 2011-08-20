67a774095a355d4c89abcf37e22d5cb3cbf88e5c
Ryan Davis <ryand@zenspider.com>
2011-08-20 03:11:34 -0800

    + Use new v3 api to get all issues in one request. Very fast! [git-p4: depot-paths = "//src/omnifocus-github/dev/": change = 6563]

--- lib/omnifocus/github.rb
+++ lib/omnifocus/github.rb
@@ -1,75 +1,24 @@
 require 'open-uri'
-require 'yaml'
 require 'json'
 
 module OmniFocus::Github
   VERSION = '1.1.0'
-
-  PREFIX = "GH"
-  GH_URL = "https://github.com"
-  GH_API_URL = "https://api.github.com"
-
-  def fetch url, key, ver = 2
-    base_url = "#{GH_URL}/api/v#{ver}/yaml"
-    YAML.load(URI.parse("#{base_url}/#{url}").read)[key]
-  end
-
-  def fetch3 url
-    JSON.parse(URI.parse("#{GH_API_URL}/#{url}").read)
-  end
+  PREFIX  = "GH"
 
   def populate_github_tasks
-    @filter = ARGV.shift
+    # curl -i -u "#{user}:#{pass}" "https://api.github.com/issues"
 
-    bug_db.delete_if do |k,v|
-      not k.index @filter
-    end if @filter
+    user = `git config --global github.user`.chomp
+    pass = `git config --global github.password`.chomp
+    uri  = URI.parse "https://api.github.com/issues"
+    body = uri.read :http_basic_authentication => [user, pass]
 
-    @user = user = `git config --global github.user`.chomp
-
-    # Personal projects
-    projects = fetch("repos/show/#{user}", "repositories").select { |project|
-      project[:open_issues] > 0
-    }.map { |project|
-      project[:name]
-    }
-
-    projects.sort.each do |project|
-      populate_issues_for user, project
-    end
-
-    # Organization projects
-    fetch("user/show/#{user}/organizations", "organizations").each do |org|
-      login = org["login"]
-      projects = fetch("repos/show/#{login}", "repositories")
-      projects = projects.select { |project| project[:open_issues] > 0}
-      projects = projects.map{|project| project[:name]}
-
-      projects.sort.each do |project|
-        populate_issues_for login, project
-      end
-    end
-  end
-
-  def populate_issues_for user_org, project
-    return unless project.index @filter if @filter
-
-    warn "  #{user_org}/#{project}"
-
-    fetch3("repos/#{user_org}/#{project}/issues").each do |issue|
-      next unless issue["assignee"]
-
+    JSON.parse(body).each do |issue|
       number    = issue["number"]
-      t_user    = begin
-                    issue["assignee"]["login"]
-                  rescue NoMethodError
-                    pp issue
-                  end
+      url       = issue["html_url"]
+      project   = url.split(/\//)[-3]
       ticket_id = "#{PREFIX}-#{project}##{number}"
       title     = "#{ticket_id}: #{issue["title"]}"
-      url       = "#{GH_URL}/#{user_org}/#{project}/issues/#{number}"
-
-      next unless t_user == @user
 
       if existing[ticket_id] then
         bug_db[existing[ticket_id]][ticket_id] = true
